9e845e71e7af80a1d4868438bbf095ef8b3ee48f
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2022-04-28 12:07:52 +0200

    Add servo2 class

--- src/Arduino/smart_cm/smart_cm.ino
+++ src/Arduino/smart_cm/smart_cm.ino
@@ -4,11 +4,15 @@
 #include "task.h"
 #include "display_task.h"
 #include "selection_task.h"
+#include "servo_motor_impl.h"
 
 Sonar* sonar;
 Button* bUp;
 Button* bDown;
 Scheduler sched;
+ServoMotor* servo;
+int pos;
+int delta;
 
 void setup() {
   Serial.begin(9600);
@@ -16,7 +20,7 @@ void setup() {
   sonar = new SonarImpl(7,8);
   float distance = sonar->getDistance();
   Serial.println(distance);*/
-  sched.init(50);
+  /*sched.init(50);
    
   Task* t0 = new DisplayTask();
   t0->init(50);
@@ -24,9 +28,25 @@ void setup() {
 
   Task* t1 = new SelectionTask(7,8,9, A0);
   t1->init(100);
-  sched.addTask(t1);
+  sched.addTask(t1);*/
+
+  servo = new ServoMotorImpl(3);
+  
+  pos = 0;
+  delta = 1;
 }
 
 void loop() {
-  sched.schedule();
+  //sched.schedule();
+  servo->on();
+  for (int i = 0; i < 180; i++) {
+    //Serial.println(pos);
+    servo->setPosition(pos);         
+    delay(5);            
+    pos += delta;
+  }
+  servo->off();
+  pos -= delta;
+  delta = -delta;
+  delay(1000);
 }
